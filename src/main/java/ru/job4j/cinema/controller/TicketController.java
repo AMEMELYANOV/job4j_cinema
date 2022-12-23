efbd71cdb94fb551e7fe4f27aa15aee35da4ce1b
AMEMELYANOV <emelyanov.am@gmail.com>
2022-12-23 15:53:41 +0300

    Рефакторинг bookTicket.html, successful.html и TicketController

--- src/main/java/ru/job4j/cinema/controller/TicketController.java
+++ src/main/java/ru/job4j/cinema/controller/TicketController.java
@@ -2,18 +2,12 @@ package ru.job4j.cinema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.job4j.cinema.model.Show;
 import ru.job4j.cinema.service.ImplShowService;
-import ru.job4j.cinema.util.UserUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Controller
 public class TicketController {
@@ -23,29 +17,28 @@ public class TicketController {
         this.showService = showService;
     }
 
-//    @GetMapping("/showRow{showId}")
-//    public String getShowRow(@RequestParam(value = "showId") Integer showId,
-//                        Model model, HttpServletRequest req) {
-//        Optional<Show> showFromDB = showService.findById(showId);
-//        model.addAttribute("show", showFromDB
-//                .orElseThrow(() -> new NoSuchElementException("Сеанс не найден")));
-//        model.addAttribute("user", UserUtil.getSessionUser(req));
-//
-//        model.addAttribute("rows", List.of(1, 2, 3, 4, 5, 6, 7));
-//
-//        HttpSession session = req.getSession();
-//        session.setAttribute("show", showFromDB.get());
-//
-//        return "showRow";
-//    }
-//
     @PostMapping("/bookTicket")
-    public String bookTicket(@RequestParam(value = "cell") Integer posRow,
+    public String bookTicket(@RequestParam(value = "cell") Integer cell,
                         Model model, HttpServletRequest req) {
         HttpSession session = req.getSession();
-        session.setAttribute("posRow", posRow);
+        session.setAttribute("cell", cell);
         model.addAttribute("show", session.getAttribute("show"));
-        model.addAttribute("cells", List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        model.addAttribute("posRow", session.getAttribute("posRow"));
+        model.addAttribute("cell", session.getAttribute("cell"));
         return "bookTicket";
+    }
+
+    @PostMapping("/cancelBuyTicket")
+    public String cancelBuyTicket() {
+        return "redirect:/index";
+    }
+
+    @PostMapping("/confirmBuyTicket")
+    public String confirmBuyTicket(Model model, HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        model.addAttribute("show", session.getAttribute("show"));
+        model.addAttribute("posRow", session.getAttribute("posRow"));
+        model.addAttribute("cell", session.getAttribute("cell"));
+        return "/successful";
     }
 }
